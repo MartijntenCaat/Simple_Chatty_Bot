ac7661e0b9163a5b6fcf2bbdb33c65670592d952
Martijn ten Caat <tencaat@gmail.com>
2020-04-21 15:03:08 +0200

    Stage #1: Hello! What’s your name

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,7 +1,26 @@
 package bot;
 
 public class SimpleBot {
+    private boolean isActive;
+    private String name;
+    private int yearOfBirth;
+
+    SimpleBot(String name) {
+        this.name = name;
+        this.isActive = true;
+        this.yearOfBirth = 2018;
+    }
+
+    private void runBot() {
+        System.out.println("Hello! My name is " + name + ".");
+        System.out.println("I was created in " + yearOfBirth + ".");
+        isActive = false;
+    }
+
     public static void main(String[] args) {
-        // write your code here
+        SimpleBot simpleBot = new SimpleBot("Aid");
+        while (simpleBot.isActive) {
+            simpleBot.runBot();
+        }
     }
 }
